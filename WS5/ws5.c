bae106265552ee47e1e64a1935fc1e1176c7d02e
luc <theluc04@yahoo.com>
2023-03-06 08:12:01 +0000

    added ws5.c

---
+++ WS5/ws5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main()
+{
+	pid_t pid;
+	pid = fork();
+	pid = fork();
+
+	if (pid < 0) {
+		printf("Error: fork failed\n");
+		return 1;
+	} else if (pid == 0) {
+		printf("hello world from PID %d!\n", getpid());
+	} else {
+		printf("hello world from PID %d!\n", getpid());
+		wait(NULL);
+	}
+	return 0;
+}
