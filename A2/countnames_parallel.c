6257be07e8bf2fe6e17cda526f0bccb7a5e20e0a
luc <theluc04@yahoo.com>
2023-03-06 23:02:37 +0000

    testing fork and pipe

--- A2/countnames_parallel.c
+++ A2/countnames_parallel.c
@@ -10,6 +10,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 /**
  * This function counts and prints names from a file.
@@ -26,63 +28,89 @@
  * Prints: "Names: Occurences"
 **/
 
+typedef struct my_data {
+  char name[30];
+  int count;
+} my_data;
+
 int main(int argc, char *argv[]) {
 
     //checks if no file or other parameters are specified
     if (argc < 2) {
         return(0);
     }
-
-    //checks if file is inputted and opens if file exists
-    FILE *fp = fopen(argv[1], "r");
-    if (fp == NULL) {
-        printf("cannot open file\n");
-        exit(1);
-    }
-    //define 100x30 array of characters and integers, integer count to keep track of array fill size
-    char names [100][31];
-    int counts [100];
-    int nameCount = 0;
-
-    //line buffer
-    char line[31];
-    //count of current line of file on
-    int lineCount = 1;
-
-    //This while loop iterates through each line of the file
-    while (fgets(line, sizeof(line), fp) != NULL) {
-        //The if else block determines whether the current line in the file is blank or contains a name
-        if (line[0] ==  '\n') {
-            fprintf(stderr, "Warning - Line %d is empty.\n", lineCount);
-        } else {
-            //Removes trailing new line character from string
-            line[strcspn(line, "\n")] = 0;
-            int found = 0;
-
-            //This for loop loops through the array name until a match is seen
-            for (int i = 0; i < 100; i++) {
-                //Check for match and increments unique name occurence, then for loop breaks
-                if (strcmp(names[i], line) == 0){
-                    counts[i]++;
-                    found = 1;
-                    break;
-                }
-            }
-            //Adds name to array if match is not found in array
-            if (found != 1) {
-                strcpy(names[nameCount], line);
-                counts[nameCount] = 1;
-                nameCount++;
-            }
+    for (int i = 1; i < argc; i++) {
+        int fd[2];
+        if (pipe(fd) == -1) {
+            printf("pipe failed");
+            exit(0);
         }
-        lineCount++;
-    }
-    fclose(fp);
 
-    //This for loop prints the occurences of each name found in file
-    for (int i = 0; i < nameCount; i++) {
-        printf("%s: %d\n", names[i], counts[i]);
+        pid_t pid;
+        pid = fork();
+
+        if (pid < 0) {
+            printf("fork failed");
+        } else if (pid == 0) {
+            printf("%s\n", argv[i]);
+            //checks if file is inputted and opens if file exists
+            FILE *fp = fopen(argv[i], "r");
+            if (fp == NULL) {
+                printf("cannot open file: %s\n", argv[i]);
+                exit(1);
+            }
+            // while loop
+            exit(0);
+        } else {
+            wait(NULL);
+        }
     }
+    
+    
+    // //define 100x30 array of characters and integers, integer count to keep track of array fill size
+    // char names [100][31];
+    // int counts [100];
+    // int nameCount = 0;
+
+    // //line buffer
+    // char line[31];
+    // //count of current line of file on
+    // int lineCount = 1;
+
+    // //This while loop iterates through each line of the file
+    // while (fgets(line, sizeof(line), fp) != NULL) {
+    //     //The if else block determines whether the current line in the file is blank or contains a name
+    //     if (line[0] ==  '\n') {
+    //         fprintf(stderr, "Warning - Line %d is empty.\n", lineCount);
+    //     } else {
+    //         //Removes trailing new line character from string
+    //         line[strcspn(line, "\n")] = 0;
+    //         int found = 0;
+
+    //         //This for loop loops through the array name until a match is seen
+    //         for (int i = 0; i < 100; i++) {
+    //             //Check for match and increments unique name occurence, then for loop breaks
+    //             if (strcmp(names[i], line) == 0){
+    //                 counts[i]++;
+    //                 found = 1;
+    //                 break;
+    //             }
+    //         }
+    //         //Adds name to array if match is not found in array
+    //         if (found != 1) {
+    //             strcpy(names[nameCount], line);
+    //             counts[nameCount] = 1;
+    //             nameCount++;
+    //         }
+    //     }
+    //     lineCount++;
+    // }
+    // fclose(fp);
+
+    // //This for loop prints the occurences of each name found in file
+    // for (int i = 0; i < nameCount; i++) {
+    //     printf("%s: %d\n", names[i], counts[i]);
+    // }
 
     return(0);
 }
