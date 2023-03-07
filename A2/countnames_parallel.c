4f6a5e68a28636846933890f50b8cd5065c1b7b9
Karan <freekaran@gmail.com>
2023-03-06 23:32:36 -0800

    Adding comments to countnames_parallel.c

--- A2/countnames_parallel.c
+++ A2/countnames_parallel.c
@@ -13,8 +13,15 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+//Defining my_data type structure that contains a name and the count of occurences.
+typedef struct my_data {
+  char name[30];
+  int count;
+} my_data;
+
+
 /**
- * This function counts and prints names from a file.
+ * This function counts and prints names for one or more files.
  * Assumption:
  * 1. The longest name is 30 characters long.
  * 2. The file is atmost 100 lines long.
@@ -23,19 +30,13 @@
  * 5. Names contain simple characters of Ascii values from 0-128.
  * 6. Input file contains only valid characters.
  *
- * Input parameters: file1
+ * Input parameters: argc, argv[]
  * Returns: int
  * Prints: "Names: Occurences"
 **/
 
-typedef struct my_data {
-  char name[30];
-  int count;
-} my_data;
-
 int main(int argc, char *argv[]) {
 
-    //test new line
     //checks if no file or other parameters are specified
     if (argc < 2) {
         return(0);
@@ -43,29 +44,30 @@ int main(int argc, char *argv[]) {
     my_data totalcounts[100]={ { '\0', 0 } };
     int nameIndex = 0;
 
+    //Loops through each file inputted to create pipe and fork child process that counts name occurences.
     for (int i = 1; i < argc; i++) {
         int fd[2];
+        //Instantiate pipe and checks if creation of pipe fd fails and exits.
         if (pipe(fd) == -1) {
             printf("pipe failed");
             exit(0);
         }
-
         pid_t pid;
         pid = fork();
 
+        //This check if the fork fails, if the fork fails it exits program.
         if (pid < 0) {
             printf("fork failed");
             exit(0);
         } else if (pid == 0) {
-            //child process
-            //checks if file is inputted and opens if file exists
+            //Enter child process
             FILE *fp = fopen(argv[i], "r");
+            //checks if file is inputted and opens if file exists
             if (fp == NULL) {
                 printf("cannot open file: %s\n", argv[i]);
                 exit(1);
             }
-            // while loop
-
+            //creates array of 100 my_data types and initalizes it to null and 0
             my_data namecounts[100]={ { '\0', 0 } };
             //line buffer
             char line[31];
@@ -103,48 +105,50 @@ int main(int argc, char *argv[]) {
             }
             fclose(fp);
 
-
-
-            close(fd[0]);
-            write(fd[1], &namecounts, sizeof(my_data)*100);
-            close(fd[1]);
+            close(fd[0]);      //Close reading end of the pipe
+            write(fd[1], &namecounts, sizeof(my_data)*100);   //Write namecounts array to pipe
+            close(fd[1]);                      //Close writing end of pipe
 
             exit(0);
-        } else { //parent process
-            wait(NULL);
+        } else {
+            //Entering parent process
+            wait(NULL);        //Wait for children to terminate
             my_data namecounts[100]={ { '\0', 0 } };
-            close(fd[1]);
+            close(fd[1]);       //Close writing end of pipe
 
-            int testCount = 0;
-                read(fd[0], namecounts, sizeof(my_data)*100);
-                for (int k = 0; k < 100; k++) {
-                    int found = 0;
-                    for (int j = 0; j < nameIndex; j++) {
-                        if (namecounts[k].count != 0) {
-                            if (strcmp(namecounts[k].name, totalcounts[j].name) == 0){
-                                totalcounts[j].count += namecounts[k].count;
-                                found = 1;
-                                break;
-                            }
+            //Reads from pipe buffer the most recent file in consecutive order
+            read(fd[0], namecounts, sizeof(my_data)*100);
+            //This loops over the array read from the pipe
+            for (int k = 0; k < 100; k++) {
+                int found = 0;
+                //This loops through the previously seen names
+                for (int j = 0; j < nameIndex; j++) {
+                    //Checks to only read non-empty entries
+                    if (namecounts[k].count != 0) {
+                        //Checks if name was seen before and sums counts together
+                        if (strcmp(namecounts[k].name, totalcounts[j].name) == 0){
+                            totalcounts[j].count += namecounts[k].count;
+                            found = 1;       //Name was seen before
+                            break;
                         }
-                    }
-
-                    if (found != 1) {
-                        if (namecounts[k].count != 0) {
-                           strcpy(totalcounts[nameIndex].name, namecounts[k].name);
-                            totalcounts[nameIndex].count = namecounts[k].count;
-                            nameIndex++;
-                        }
-
                     }
                 }
-
+                //Name was not seen before
+                if (found != 1) {
+                    //Checks to only read non-empty entries
+                    if (namecounts[k].count != 0) {
+                        //Copys new name from read array to total count array.
+                        strcpy(totalcounts[nameIndex].name, namecounts[k].name);
+                        totalcounts[nameIndex].count = namecounts[k].count;
+                        nameIndex++;
+                    }
+                }
+            }
         }
-
     }
-    
+    //This loops through the aggregated counts and prints out the number of occurences of each name.
     for (int w = 0; w < nameIndex; w++) {
-        printf("%d %s: %d\n", w, totalcounts[w].name, totalcounts[w].count);
+        printf("%s: %d\n", totalcounts[w].name, totalcounts[w].count);
     }
 
     return(0);
