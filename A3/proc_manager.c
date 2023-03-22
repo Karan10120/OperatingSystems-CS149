7191e0a736bf0b88825efc0ea937997f64896471
Karan <freekaran@gmail.com>
2023-03-22 15:32:55 -0700

    Printing with fflush

--- A3/proc_manager.c
+++ A3/proc_manager.c
@@ -33,46 +33,59 @@ int main() {
         if ((pid = fork()) < 0) {
             printf("fork error");
             exit(1);
-        } else if (pid == 0) { /* child */
+
+        } else if (pid == 0) { //child
             // creating output and error files
             // char filenameout[10], filenameerr[10];
             // int fd_out, fd_err;
 
-
             sprintf(filenameout, "%d.out", getpid()); 
             fd_out = open(filenameout, O_RDWR | O_CREAT | O_APPEND, 0777);
-            fp = fopen(filenameout, "a");
             dup2(fd_out, 1);
+
+            printf("Starting command %d: child %d pid of parent %d\n", cmd_count++, getpid(), getppid());
+            fflush(stdout);
             close(fd_out);
+
+            //fflush(stdout);
+            //printf("About to run:");
+            //fflush(stdout);
+
+            //for (int i = 0; i < (sizeof(args)/sizeof(args[0])); i++){
+            //printf("%d Parameter: %s", i, args[i]);
+            //}
 
             execvp(args[0], args);
             // TODO: increment cmd_count somewhere
             //       print couldn't execute to .err instead of .out
             //       fix print starting command line to .out file
-            fprintf(fp, "starting command %d: child %d PID of parent %d", cmd_count, getpid(), getppid());
-
+            //fprintf(fp, "starting command %d: child %d PID of parent %d", cmd_count, getpid(), getppid());
+        //Problem: blank line enter is returning a WIFSIGNALED, causing it to print Killed with sig statement
             sprintf(filenameerr, "%d.err", getpid());
             fd_err = open(filenameerr, O_RDWR | O_CREAT | O_APPEND, 0777);
             dup2(fd_err, 2);
             close(fd_err);
             
-            printf("couldn't execute: %s\n", args[0]);
+            printf("Could not execute command: %s\n", args[0]);
+            fflush(stderr);
             exit(2);
         }
+
+
         /* parent */
         while ((pid = wait(&status)) > 0) {
             sprintf(filenameerr, "%d.err", pid);
             fp = fopen(filenameerr, "a");
             if (WIFEXITED(status)) {
-                fprintf(fp, "exited with exit code = %d\n", WEXITSTATUS(status));
+                fprintf(fp, "Exited with exit code = %d\n", WEXITSTATUS(status));
             } else if (WIFSIGNALED(status)) {
-                fprintf(fp, "killed with signal %d\n", WTERMSIG(status));
+                fprintf(fp, "Killed with signal %d\n", WTERMSIG(status));
             }
             fclose(fp);
 
             sprintf(filenameout, "%d.out", pid);
             fp = fopen(filenameout, "a");
-            fprintf(fp, "finished child %d PID of parent %d\n", pid, getpid());
+            fprintf(fp, "Finished child %d PID of parent %d\n", pid, getpid());
             fclose(fp);
         }
     }
